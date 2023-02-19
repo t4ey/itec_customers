4eb919ea913c42023745917f0f77b9234d993e22
XxExecut0rxX <alva200rey@gmail.com>
2023-02-19 19:55:19 +0000

    initializing git first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express'); 
+const app = express();
+const port = 3000;
+
+const path = require('path');
+
+//MySQL connection
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host: '192.168.76.200',
+    user: 'itec',
+    password: 'alva234',
+    database: 'ova_store'
+});
+
+db.connect(
+    (error) => {
+        if(error)
+            console.log(error);
+        else console.log("MySQL conection success.....");
+    }
+)
+
+// HBS home page with handle bars
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+
+app.set('view engine', 'hbs');
+
+
+app.get("/", (req, res) => {
+    // res.send("Home Page");
+    res.render('index.hbs');
+});
+
+app.listen(port, () => {
+    console.log("server started at port: "  + port);
+});
