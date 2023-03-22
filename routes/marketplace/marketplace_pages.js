81f5f0b80007586bec3b9f895d28c32b134279f6
XxExecut0rxX <alva200rey@gmail.com>
2023-03-22 22:14:34 +0000

    marketplace/product/id page with the button add_to_cart working great

---
+++ routes/marketplace/marketplace_pages.js
@@ -0,0 +1,44 @@
+const { db } = require("../../database/database");
+
+exports.add_to_shopping_cart = async (req, res) => {
+    const { id } = req.params;
+    const product_id = id;
+    const client_id = res.locals.user.id;
+    console.log('id : ', client_id)
+    console.log('p:id : ', product_id)
+    const { quantity } = req.body;
+
+    console.log(quantity);
+
+    if (!quantity || (quantity > 50) || (quantity < 0) || !client_id) {
+        req.flash('message', "Algo salio mal");
+        req.flash('alertType', "alert-warning");
+        return res.redirect('/marketplace/product/' + product_id);
+    }
+
+    await db.query("SELECT * FROM cart_shopping WHERE client_id = ? AND producto_id = ?", [client_id, product_id], async (error, result) => {
+        if (error)
+            console.log(error);
+
+        // console.log('result', result);
+        if (result.length > 0) {
+            req.flash('message', "Ya ha añadido este producto al carrito de compras");
+            req.flash('alertType', "alert-danger");
+            return res.redirect('back');
+        }
+
+        db.query('INSERT INTO cart_shopping SET ?', { client_id: client_id, producto_id: product_id, cantidad: quantity }, (error, result) => {
+            if (error)
+                console.log(error);
+
+            else {
+                req.flash('message', "Producto añadido.");
+                req.flash('alertType', "alert-success");
+                return res.redirect('/marketplace/product/' + product_id);
+            }
+
+        });
+    });
+
+    // res.send("ok");
+}
--- routes/pages.js
+++ routes/pages.js
@@ -5,7 +5,8 @@ const router = express.Router();
 
 // authentication jwt
 const { requireAuth, checkUser, alreadyLogged } = require('../middleware/customers_middleware.js');
-const { products } = require('./admin_tables/product_tables.js');
+// const { products } = require('./admin_tables/product_tables.js');
+const marketplace = require('./marketplace/marketplace_pages.js');
 
 // GET ROUTES
 
@@ -47,14 +48,14 @@ router.get('/marketplace', requireAuth, async (req, res) => {
 
 router.get('/marketplace/product/:id', requireAuth, async (req, res) => {
     const { id } = req.params;
-    console.log('id : ', id)
+    // console.log('id : ', id)
     const message = req.flash('message');
     const alertType = req.flash('alertType');
 
     let product = await db.query("SELECT * FROM producto WHERE id = ?", [id]);
     let categories = await db.query("SELECT * FROM categoria");
 
-    console.log(product);
+    // console.log(product);
     // console.log(categories);
     res.render('./marketplace/product.hbs', {
         categories: categories,
@@ -66,19 +67,19 @@ router.get('/marketplace/product/:id', requireAuth, async (req, res) => {
 
 
 router.get('/marketplace/cart_shopping', requireAuth, async (req, res) => {
-    const { id } = req.params;
-    console.log('id : ', id)
+    // const  user_id = res.locals.user.id;
+    // console.log('id : ', user_id)
     const message = req.flash('message');
     const alertType = req.flash('alertType');
 
-    let product = await db.query("SELECT * FROM producto WHERE id = ?", [id]);
-    let categories = await db.query("SELECT * FROM categoria");
+    // let product = await db.query("SELECT * FROM  WHERE id = ?", [id]);
+    // let categories = await db.query("SELECT * FROM categoria");
 
-    console.log(product);
+    // console.log(product);
     // console.log(categories);
     res.render('./marketplace/cart_shopping.hbs', {
-        categories: categories,
-        product: product[0],
+        // categories: categories,
+        // product: product[0],
         message: message,
         alertType: alertType,
     });
@@ -94,5 +95,9 @@ router.post('/login', alreadyLogged, authController.login);
 router.post('/profile', checkUser, authController.profile);
 
 router.get('/logout', authController.logout);
+
+    // marketplace posts
+
+router.post('/marketplace/cart_shopping/add_product/:id', checkUser, marketplace.add_to_shopping_cart);
 
 module.exports = router;
